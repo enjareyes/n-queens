d650072dfae4eb6b1fa6ac14c96ea8c18455f3cb
E N J A <enja.reyes@gmail.com>
2015-06-09 10:36:35 -0500

    Update solvers.js

--- src/solvers.js
+++ src/solvers.js
@@ -1,26 +1,17 @@
-/*           _
-   ___  ___ | |_   _____ _ __ ___
-  / __|/ _ \| \ \ / / _ \ '__/ __|
-  \__ \ (_) | |\ V /  __/ |  \__ \
-  |___/\___/|_| \_/ \___|_|  |___/
-
-*/
-
 // hint: you'll need to do a full-search of all possible arrangements of pieces!
 // (There are also optimizations that will allow you to skip a lot of the dead search space)
 // take a look at solversSpec.js to see what the tests are expecting
-
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, 
 // with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var counter = 0;  
-  var solution;
-  var board = new Board({n:n})
+  var counter = 0,
+      solution,
+      board = new Board({n:n});
 
   var recursiveFunc = function(initRow){
-    for (var col=0; col < n; col++){
+    for (var col = 0; col < n; col++){
       board.togglePiece(initRow, col);
 
       if (board.hasAnyRooksConflicts()){
@@ -41,17 +32,14 @@ window.findNRooksSolution = function(n) {
 };
 
 
-
-// return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
+// return the number of nxn chessboards that exist, 
+// with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = 0
-  var board = new Board({n:n});
+  var solutionCount = 0,
+      board = new Board({n:n});
 
   var recursiveFunc = function(initRow){
-    if(initRow===n){
-      solutionCount++
-      return
-    }
+    if(initRow===n) return solutionCount++ ;
 
     for (var col = 0; col < n; col++){
       board.togglePiece(initRow, col);
@@ -67,27 +55,19 @@ window.countNRooksSolutions = function(n) {
 };
 
 
-
 // return a matrix (an array of arrays) representing a single nxn chessboard, 
 // with n queens placed such that none of them can attack each other
 
 window.findNQueensSolution = function(n) { 
+  var solution = [],
+      board = new Board({n:n});
 
-  var solution = [];
-  var board = new Board({n:n});
-
-  if (n === 1) {
-    return [[1]]
-  }
-
-  if (n === 2 || n === 3) {
-    return board.rows();
-  }
+  if (n === 1) return [[1]];
+  if (n === 2 || n === 3) return board.rows();
 
   var recursiveFunc = function(initRow){
 
     if(initRow === n){
-      console.log(solution)
       solution = _.map(board.rows(), function(row){ return row.slice()})
       return 
     }
@@ -105,8 +85,6 @@ window.findNQueensSolution = function(n) {
 
   recursiveFunc(0)
   return solution
-
-
 };
 
 
@@ -114,20 +92,16 @@ window.findNQueensSolution = function(n) {
 // with n queens placed such that none of them can attack each other
 
 window.countNQueensSolutions = function(n) {
-  var solutionCount = 0
-  var board = new Board({n:n});
+  var solutionCount = 0,
+      board = new Board({n:n});
 
   if (n === 2 || n === 3) {
     solutionCount = 0;
-    console.log('solution count: ' + solutionCount)
     return solutionCount
   }
 
   var recursiveFunc = function(initRow){
-    if(initRow===n){
-      solutionCount++
-      return
-    }
+    if(initRow === n) return solutionCount++ ;
 
     for (var col = 0; col < n; col++){
       board.togglePiece(initRow, col);
